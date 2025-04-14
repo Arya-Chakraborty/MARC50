4e502e52ed9470b13cf10c04c5c1afe4f526babf
Arya-Chakraborty <aryastlawrence@gmail.com>
2025-04-14 15:07:13 +0530

    render config update

--- api/index.py
+++ api/index.py
@@ -40,4 +40,4 @@ def predict():
     
 if __name__ == "__main__":
     port = int(os.environ.get("FLASK_PORT", 5328))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, threaded=True)  # Added threaded
